c950db322bd37ae952c0a854bca8fa44370f0a7f
viblazer <golafoned@gmail.com>
2025-05-19 21:28:46 +0300

    minor

--- scripts/download-files.sh
+++ scripts/download-files.sh
@@ -4,7 +4,7 @@ mkdir -p data
 cd data
 
 # get En-Uk training data for WMT17
-wget -nc https://drive.usercontent.google.com/download?id=1pS5pM0H8n4383saEuf-3yQtOquu5Uawj&export=download&confirm=t
+wget -O training.tgz "https://drive.usercontent.google.com/download?id=1pS5pM0H8n4383saEuf-3yQtOquu5Uawj&export=download&confirm=t"
 
 # extract data
 tar -xf training.tgz
