ea3d5f217c47d3ebca5cb0e82b35b1a313b61ae2
viblazer <golafoned@gmail.com>
2025-05-19 21:53:18 +0300

    minor

--- scripts/download-files.sh
+++ scripts/download-files.sh
@@ -3,14 +3,30 @@
 mkdir -p data
 cd data
 
-# get En-Uk training data for WMT17
-wget -nc -O training.tgz "https://drive.usercontent.google.com/download?id=1pS5pM0H8n4383saEuf-3yQtOquu5Uawj&export=download&confirm=t"
+wget -nc http://data.statmt.org/wmt17/translation-task/training-parallel-nc-v12.tgz
 
 # extract data
-tar -xf training.tgz
+tar -xf training-parallel-nc-v12.tgz
 
 # create corpus files
-cat training/corpus.uk-en.uk > corpus.uk
-cat training/corpus.uk-en.en > corpus.en
+cat training/news-commentary-v12.de-en.de > corpus.uk
+cat training/news-commentary-v12.de-en.en > corpus.en
+
+# clean
+rm -r europarl-* commoncrawl.* training/ *.tgz
 
 cd ..
+
+
+
+# # get En-Uk training data for WMT17
+# wget -nc -O training.tgz "https://drive.usercontent.google.com/download?id=1pS5pM0H8n4383saEuf-3yQtOquu5Uawj&export=download&confirm=t"
+
+# # extract data
+# tar -xf training.tgz
+
+# # create corpus files
+# cat training/corpus.uk-en.uk > corpus.uk
+# cat training/corpus.uk-en.en > corpus.en
+
+# cd ..
