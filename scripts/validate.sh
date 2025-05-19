4ed88c508d3a9a5749e662810c161ea4526512a8
viblazer <golafoned@gmail.com>
2025-05-19 22:41:55 +0300

    minor

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,6 +3,6 @@
 cat $1 \
     | sed 's/\@\@ //g' \
     | ../tools/moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
-    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l uk 2>/dev/null \
+    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l de 2>/dev/null \
     | ../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/valid.uk \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
